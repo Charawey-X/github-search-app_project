add97abe7ac8101f43cc9668194b1c0704b08664
Charawey-X <charawey@gmail.com>
2022-04-16 16:38:13 +0300

    edited filter

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value:any, filterString: string) {
+    if (value.length == 0 || filterString == '') return value;
+
+    const repos = []
+    for (const repo of value){
+      if (repo['name'].toLowerCase().includes(filterString.toLowerCase())){
+        repos.push(repo);
+      }
+    }
+    return repos;
   }
 
 }
